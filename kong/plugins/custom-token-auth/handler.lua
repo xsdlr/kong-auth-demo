1776cbedf3c73671829435339f517426c10bffb6
xsdlr <xsdlr123@gmail.com>
2017-07-07 16:09:26 +0800

    添加调试日志

--- kong/plugins/custom-token-auth/handler.lua
+++ kong/plugins/custom-token-auth/handler.lua
@@ -67,6 +67,8 @@ end
 function TokenAuthHandler:access(conf)
   TokenAuthHandler.super.access(self)
   local is_options_request = ngx.req.get_method == ngx.HTTP_OPTIONS
+  ngx.log(ngx.DEBUG, "request method: ", ngx.req.get_method)
+  ngx.log(ngx.DEBUG, "request is options: ", is_options_request)
   if is_options_request then
     return responses.send_HTTP_OK()
   end
